2b2bd09e5ad0e93326abe24d5e07b4425a26beb7
wiyco <me@mail.wiyco.dev>
2024-06-20 02:21:49 +0900

    chore(next): add webpack config of svgr

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,28 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
+  /** @see {@link https://nextjs.org/docs/pages/api-reference/next-config-js/webpack} */
+  webpack: (config) => {
+    /** @see {@link https://react-svgr.com/docs/next} */
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
 };
 
 export default nextConfig;
