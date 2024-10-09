47e0303a3c893ca10bc7c7bec1abd5441be91395
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-09 20:09:40 +0900

    refactor(providers): remove nextui provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 
-/** @see {@link https://nextui.org/docs/guide/installation#manual-installation} */
 /** @see {@link https://github.com/pacocoursey/next-themes} */
 
 export function Providers(props: {
@@ -12,16 +10,10 @@ export function Providers(props: {
     React.ComponentProps<typeof NextThemeProvider>,
     "children"
   >;
-  nextUIProviderProps?: Omit<
-    React.ComponentProps<typeof NextUIProvider>,
-    "children"
-  >;
 }) {
   return (
-    <NextUIProvider {...props.nextUIProviderProps}>
-      <NextThemeProvider {...props.nextThemeProviderProps}>
-        {props.children}
-      </NextThemeProvider>
-    </NextUIProvider>
+    <NextThemeProvider {...props.nextThemeProviderProps}>
+      {props.children}
+    </NextThemeProvider>
   );
 }
