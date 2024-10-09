04ee47a82f465dbbadd6aab73cd4b54670f22c75
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-09 19:32:09 +0900

    chore(next): remove webpack config related to svgr

--- next.config.mjs
+++ next.config.mjs
@@ -10,40 +10,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
-  /** @see {@link https://nextjs.org/docs/pages/api-reference/next-config-js/webpack} */
-  webpack: (config) => {
-    /** @see {@link https://react-svgr.com/docs/next} */
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: "@svgr/webpack",
-            /** @see {@link https://react-svgr.com/docs/options} */
-            options: {
-              icon: "1.5em",
-              memo: true,
-              replaceAttrValues: {
-                "#000": "currentColor",
-              },
-            },
-          },
-        ],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
-  },
 };
 
 export default nextConfig;
