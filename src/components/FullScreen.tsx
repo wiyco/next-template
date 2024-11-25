91ca9154b2dfc9e1f15e292497ba5b40653df6ed
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-25 12:58:53 +0900

    delete: unused components

--- src/components/FullScreen.tsx
+++
@@ -1,7 +0,0 @@
-export function FullScreen({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="grid h-dvh w-full place-content-center text-neutral-800 dark:text-neutral-300">
-      {children}
-    </main>
-  );
-}
