f5b6bc4c9646e1b58c823aaff275c48efb0b58eb
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-25 12:18:23 +0900

    refactor(cn): add docs

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,11 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Merges class names and tailwind classes.
+ * @param classNames - Class names to merge.
+ * @returns Merged class names.
+ */
 export function cn(
   ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
 ): string {
