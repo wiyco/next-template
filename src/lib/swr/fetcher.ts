5b8dbd3825d9a3d3125dbd90e5e4c1f325432c73
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-28 06:33:52 +0900

    refactor(swr): named export

--- src/lib/swr/fetcher.ts
+++ src/lib/swr/fetcher.ts
@@ -3,6 +3,6 @@
  * @param key - URL to fetch data from
  * @see {@link https://swr.vercel.app}
  */
-export default async function fetcher(key: string) {
+export async function fetcher(key: string) {
   return fetch(key).then((res) => res.json());
 }
