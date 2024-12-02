1d85dc0e00d52be5cd329cf548cfb125cb1c77d0
wiyco <72733890+wiyco@users.noreply.github.com>
2024-12-02 20:37:02 +0900

    feat(types): add types of next.js related

---
+++ src/types/next.ts
@@ -0,0 +1,29 @@
+/**
+ * Next.js dynamic route segments.
+ * @example
+ * ```tsx
+ * import type { DynamicSegments } from "@/types";
+ * export default async function Page({ params }: DynamicSegments<"slug">) {
+ *   console.log(params.slug);
+ *   // => "foo"
+ * }
+ * ```
+ * @see {@link https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes}
+ */
+export type DynamicSegments<T extends string = "slug"> = {
+  params: { [K in T]: string };
+};
+
+/**
+ * Next.js search parameters.
+ * @example
+ * ```tsx
+ * import type { SearchParams } from "@/types";
+ * export default async function Page({ searchParams }: { searchParams: SearchParams }) {
+ *   console.log(searchParams.foo);
+ *   // => "bar"
+ * }
+ * ```
+ * @see {@link https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional}
+ */
+export type SearchParams = { [key: string]: string | string[] | undefined };
