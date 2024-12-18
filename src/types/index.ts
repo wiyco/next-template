b94b528163b083923635ef89ef599cbaed2cc437
wiyco <72733890+wiyco@users.noreply.github.com>
2024-12-18 22:20:36 +0900

    feat(types): add utility types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export type * from "./next";
+export type * from "./utils";
---
+++ src/types/utils.ts
@@ -0,0 +1,24 @@
+/**
+ * Don't use `{}` as a type. `{}` actually means "any non-nullish value".
+ * * If you want a type meaning "any object", you probably want `object` instead.
+ */
+export type AnyObject = Record<string, unknown>;
+
+/**
+ * Don't use `{}` as a type. `{}` actually means "any non-nullish value".
+ * * If you want a type meaning "empty object", you probably want `Record<string, never>` instead.
+ */
+export type EmptyObject = Record<string, never>;
+
+/**
+ * Don't use `{}` as a type. `{}` actually means "any non-nullish value".
+ * * If you really want a type meaning "any non-nullish value", you probably want `NonNullable<unknown>` instead.
+ */
+export type StrictNonNullable = NonNullable<unknown>;
+
+/**
+ * Merge two types.
+ * * If `N` is an object, merge `M` and `N`.
+ * * If `N` is not an object, return `M`.
+ */
+export type Merge<M, N> = N extends AnyObject ? M : Omit<M, keyof N> & N;
